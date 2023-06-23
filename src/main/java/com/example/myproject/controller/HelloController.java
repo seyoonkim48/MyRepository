bd2ea4cdf02b6db5d7f12648693afd7a733ed889
gimseyun <seyoonkim48@gmail.com>
2023-06-23 10:16:39 +0900

    도대체 언제부터 된건데

--- src/main/java/com/example/myproject/controller/HelloController.java
+++ src/main/java/com/example/myproject/controller/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String hello() {
-        return "Hello World!! 오예!";
+        return "Hello World!! 오예! 도대체 언제부터 된건데...";
     }
 }
